165fcdc13fc770f6fa1346a178022aba77eb83dd
vishalscodes <104351431+vishalscodes@users.noreply.github.com>
2023-01-09 15:59:36 +0530

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("all-articles");
 
   return {
     dir: {
