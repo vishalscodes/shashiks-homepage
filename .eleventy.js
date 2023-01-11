8e41222938cf304a5a24c10b443317bb187340a1
vishalscodes <104351431+vishalscodes@users.noreply.github.com>
2023-01-11 17:54:30 +0530

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const { DateTime } = require("luxon");
+const { Date } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
@@ -6,8 +6,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("all-articles");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    return Date.fromJSDate(dateObj).toLocaleString(Date.DATE_MED);
   });
+  eleventyConfig.addCollection("specialCollection", function (collection) {
+  return collection.getAll().filter((post) => post.data.post);
+});
   return {
     dir: {
       input: "html",
--- all-articles/10-01-2023-vvvvvvvvv.md
+++
@@ -1,9 +0,0 @@
----
-title: vvvvvvvvv
-description: vvvvv
-date: 2023-01-10T15:45:22.874Z
-tags:
-  - post
-author: mmmmmmm
----
-m﻿mmmmmmmmmmmm
