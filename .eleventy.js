812f6fbfe10d472aaa588bf3dbc5e0f8667ea5b2
vishalscodes <104351431+vishalscodes@users.noreply.github.com>
2023-01-13 18:30:19 +0530

    Trying to make the blog page html page work

--- .eleventy.js
+++ .eleventy.js
@@ -8,9 +8,13 @@ module.exports = function (eleventyConfig) {
     return Date.fromJSDate(dateObj).toLocaleString(Date.DATE_MED);
   });
 
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("/all-articles/**/*.md");
+  });
+
   return {
     dir: {
-      input: "html && all-articles",
+      input: "html",
     },
   };
 };
