47efea4285e3c7526336ec3edf75979ac4670427
vishalscodes <104351431+vishalscodes@users.noreply.github.com>
2023-01-15 16:24:19 +0530

    update

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const { Date } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("service-worker.js");
+  eleventyConfig.addPassthroughCopy("manifest.json");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return Date.fromJSDate(dateObj).toLocaleString(Date.DATE_MED);
