373183125aea476f8f69f9ca017e677219d6736c
vishalscodes <104351431+vishalscodes@users.noreply.github.com>
2023-01-15 20:17:48 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
 
 workbox.routing.registerRoute(
-({register}) => request.destination === 'image'),
+({request}) => request.destination === 'image';
 new workbox.strategies.NetworkFirst()
+)
